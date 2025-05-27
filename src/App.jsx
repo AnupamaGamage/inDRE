99204753cf01162aadeebe7048ffaa84b82f9ed0
Anupama Indrachapa Gamage <anx.3145@gmail.com>
2025-05-27 20:52:45 +0000

    set homepage for github

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Anupama Gamage",
+  title: "Undergraduate Entrepreneur",
+  description: "A passionate undergraduate student with a keen interest in entrepreneurship and technology.",
+  email: "anupamagamage310@gmail.com",
+  gitHub: "AnupamaGamage",
+  instagram: "itsyaboyindre",
+  linkedIn: "Anupama Gamage",
 };
 
 const primaryColor = "#4E567E";
